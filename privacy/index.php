95cb5997bb2739b107f8c576b90fe388d8a3b5f5
Sizu24 <sizu1124@gmail.com>
2023-09-30 13:05:21 -0700

    Add privacy, terms

--- privacy/index.php
+++ privacy/index.php
@@ -10,62 +10,107 @@
 <h1 class="privacy__title">Privacy Policy</h1>
 
 <p class="privacy__body">
-Last updated: September 30, 2023
+  Last updated: September 30, 2023
 </p>
 
 <p class="privacy__body">
-Thank you for visiting the Wealth Builders Empire website. We are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy outlines our practices concerning the collection, use, and protection of your information when you use our website and contact us through the provided contact form.
+  Thank you for visiting the Wealth Builders Empire website. We are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy outlines our practices concerning the collection, use, and protection of your information when you use our website and contact us through the provided contact form.
+</p>
 
-By accessing or using our website, you consent to the terms of this Privacy Policy. If you do not agree with the practices described in this policy, please do not use our website or provide any personal information through the contact form.
+<p class="privacy__body">
+  By accessing or using our website, you consent to the terms of this Privacy Policy. If you do not agree with the practices described in this policy, please do not use our website or provide any personal information through the contact form.
+</p>
 
-1. Information We Collect
+<h2 class="legal__title">
+  1. Information We Collect
+</h2>
 
-When you visit our website, we may collect two types of information:
+<p class="privacy__body">
+  When you visit our website, we may collect two types of information:
+</p>
 
-a. Personal Information: Personal information is data that can be used to identify you as an individual, such as your name, email address, phone number, and any other information you provide voluntarily through the contact form.
+<p class="privacy__body">
+  a. Personal Information: Personal information is data that can be used to identify you as an individual, such as your name, email address, phone number, and any other information you provide voluntarily through the contact form.
+</p>
 
-b. Non-Personal Information: We may collect non-personal information such as your IP address, browser type, and operating system. This information helps us understand how visitors use our website and improve its functionality.
+<p class="privacy__body">
+  b. Non-Personal Information: We may collect non-personal information such as your IP address, browser type, and operating system. This information helps us understand how visitors use our website and improve its functionality.
+</p>
 
-2. How We Use Your Information
+<h2 class="legal__title">
+  2. How We Use Your Information
+</h2>
 
-We may use the information we collect for the following purposes:
+<p class="privacy__body">
+  We may use the information we collect for the following purposes:
+</p>
 
-a. Providing Services: We use your personal information to respond to your inquiries submitted through the contact form, provide financial advisory services, and communicate with you.
+<p class="privacy__body">
+  a. Providing Services: We use your personal information to respond to your inquiries submitted through the contact form, provide financial advisory services, and communicate with you.
+</p>
 
-b. Website Improvement: Non-personal information is used for website analytics to enhance the user experience and improve our website's content and functionality.
+<p class="privacy__body">
+  b. Website Improvement: Non-personal information is used for website analytics to enhance the user experience and improve our website's content and functionality.
+</p>
 
-c. Legal Compliance: We may process your information to comply with legal obligations or to protect our rights and interests.
+<p class="privacy__body">
+  c. Legal Compliance: We may process your information to comply with legal obligations or to protect our rights and interests.
+</p>
 
-3. Data Security
+<h2 class="legal__title">
+  3. Data Security
+</h2>
 
-We take data security seriously. We employ industry-standard security measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction.
+<p class="privacy__body">
+  We take data security seriously. We employ industry-standard security measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction.
+</p>
 
-4. Cookies
+<h2 class="legal__title">
+  4. Cookies
+</h2>
 
-Our website may use cookies to improve your browsing experience. You can set your browser to refuse all cookies or to indicate when a cookie is being sent. Please note that if you choose to disable cookies, some parts of the website may not function correctly.
+<p class="privacy__body">
+  Our website may use cookies to improve your browsing experience. You can set your browser to refuse all cookies or to indicate when a cookie is being sent. Please note that if you choose to disable cookies, some parts of the website may not function correctly.
+</p>
 
-5. Third-Party Links
+<h2 class="legal__title">
+  5. Third-Party Links
+</h2>
 
-Our website may contain links to third-party websites. Please be aware that we are not responsible for the privacy practices or content of these external sites. We encourage you to review the privacy policies of these websites when you visit them.
+<p class="privacy__body">
+  Our website may contain links to third-party websites. Please be aware that we are not responsible for the privacy practices or content of these external sites. We encourage you to review the privacy policies of these websites when you visit them.
+</p>
 
-6. Your Rights
+<h2 class="legal__title">
+  6. Your Rights
+</h2>
 
-You have the right to access, correct, or delete your personal information, as well as to withdraw your consent for us to use it. If you wish to exercise any of these rights or have questions regarding your personal data, please contact us using the contact information provided below.
+<p class="privacy__body">
+  You have the right to access, correct, or delete your personal information, as well as to withdraw your consent for us to use it. If you wish to exercise any of these rights or have questions regarding your personal data, please contact us using the contact information provided below.
+</p>
 
-7. Changes to this Privacy Policy
+<h2 class="legal__title">
+  7. Changes to this Privacy Policy
+</h2>
 
-We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. The updated policy will be posted on our website, and the date of the last revision will be indicated at the top of the page.
+<p class="privacy__body">
+  We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. The updated policy will be posted on our website, and the date of the last revision will be indicated at the top of the page.
+</p>
 
-8. Contact Us
+<h2 class="legal__title">
+  8. Contact Us
+</h2>
 
-If you have any questions or concerns regarding this Privacy Policy or our data practices, please contact us at:
+<p class="privacy__body">
+  If you have any questions or concerns regarding this Privacy Policy or our data practices, please contact us.
+</p>
 
-Wealth Builders Empire
-Email Address
+<p class="privacy__body"> 
+  By using our website and submitting personal information through the contact form, you acknowledge that you have read and understood this Privacy Policy and agree to its terms.
+</p>
 
-By using our website and submitting personal information through the contact form, you acknowledge that you have read and understood this Privacy Policy and agree to its terms.
-
-Thank you for choosing Wealth Builders Empire for your financial advisory needs. Your privacy and trust are important to us.
+<p class="privacy__body">
+  Thank you for choosing Wealth Builders Empire for your financial advisory needs. Your privacy and trust are important to us.
 </p>
 </body>
 </html>
---
+++ terms-and-conditions/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wealth Builders Empire | Privacy Policy</title>
+</head>
+<body>
+ 
+<h1 class="legal__title">
+  Terms and Conditions
+</h1>
+
+<p class="legal__body">
+  Last updated: September 30, 2023
+</p>
+
+<p class="legal__body">
+  Please read these Terms and Conditions carefully before using the website of [Your Financial Advisor Firm]. By accessing or using our website, you agree to be bound by the following terms and conditions. If you do not agree to these terms, please do not use our website.
+</p>
+
+<h2 class="legal__title">
+  1. Use of Website
+</h2>
+
+<p class="legal__body">
+  a. Accuracy of Information: The content on this website is provided for general informational purposes only. We make every effort to ensure the accuracy of the information, but we do not warrant or guarantee its completeness, reliability, or suitability for your specific needs.
+</p>
+
+<p class="legal__body">
+  b. Professional Advice: The information on this website does not constitute financial, investment, legal, or tax advice. You should consult with a qualified professional before making any financial decisions.
+</p>
+
+<h2 class="legal__title">
+2. Privacy
+</h2>
+
+<p class="legal__body">
+  Your use of our website is also governed by our Privacy Policy, which can be found [here](link to your Privacy Policy page).
+</p>
+
+<h2 class="legal__title">
+3. Intellectual Property
+</h2>
+
+<p class="legal__body">
+  a. Copyright: All content on this website, including text, graphics, logos, images, and software, is the property of [Your Financial Advisor Firm] and is protected by copyright laws. You may not reproduce, distribute, modify, or create derivative works from any part of this website without our prior written consent.
+</p>
+
+<p class="legal__body">
+  b. Trademarks: Any trademarks, service marks, or logos displayed on this website are the property of [Your Financial Advisor Firm] or their respective owners. You may not use, reproduce, or display them without our written permission or the permission of the respective owner.
+</p>
+
+<h2 class="legal__title">
+4. Limitation of Liability
+</h2>
+
+<p class="legal__body">
+  a. No Warranty: This website is provided "as is" without any warranties, expressed or implied, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, or non-infringement.
+</p>
+
+<p class="legal__body">
+  b. Limitation of Liability: [Your Financial Advisor Firm] shall not be liable for any direct, indirect, incidental, special, or consequential damages resulting from your use of this website, including but not limited to lost profits or data, even if we have been advised of the possibility of such damages.
+</p>
+
+<h2 class="legal__title">
+5. External Links
+</h2>
+
+<p class="legal__body">
+  This website may contain links to external websites operated by third parties. [Your Financial Advisor Firm] has no control over the content or practices of these external sites and is not responsible for their content or privacy practices. Your use of external websites is at your own risk.
+</p>
+
+<h2 class="legal__title">
+6. Modifications
+</h2>
+
+<p class="legal__body">
+  We reserve the right to modify these Terms and Conditions at any time without notice. Any changes will be effective immediately upon posting on this website. Your continued use of the website after the posting of changes constitutes your acceptance of the modified Terms and Conditions.
+</p>
+
+<h2 class="legal__title">
+7. Governing Law
+</h2>
+
+<p class="legal__body">
+  These Terms and Conditions are governed by and construed in accordance with the laws of [Your Jurisdiction]. Any legal actions arising out of or related to these terms shall be filed in the appropriate state or federal court located in [Your Jurisdiction].
+</p>
+
+<h2 class="legal__title">
+8. Contact Information
+</h2>
+
+<p class="legal__body">
+  If you have any questions or concerns regarding these Terms and Conditions, please contact us.
+</p>
+
+<p class="legal__body">
+  Thank you for visiting the website of [Your Financial Advisor Firm]. Your use of our website is subject to these Terms and Conditions, and we appreciate your cooperation and understanding.
+</p>
+
+</body>
+</html>
