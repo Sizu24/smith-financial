a60f479cfcc08edbc2a6cefd73baca24a2cef89f
Sizu24 <sizu1124@gmail.com>
2023-09-30 18:06:39 -0700

    Add change to FB script

--- facebook/index.php
+++ facebook/index.php
@@ -2,35 +2,29 @@
 <html lang="en">
   <head></head>
   <body>
-
+  <div id="fb-root"></div>
+  <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0&appId=267770262794160" nonce="18wa1EOv"></script>
     <h2>Add Facebook Login to your webpage</h2>
+    
 
       <!-- Set the element id for the JSON response -->
   
       <p id="profile">
 
       </p>
+      <div class="fb-login-button" data-width="32px" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="true"></div>
 
       <script>
   
         //Add the Facebook SDK for Javascript
-  
-        (function(d, s, id){
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {return;}
-          js = d.createElement(s); js.id = id;
-          js.src = "https://connect.facebook.net/en_US/sdk.js";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk')
-        );
-
 
         window.fbAsyncInit = function() {
             // Initialize the SDK with your app and the Graph API version for your app 
             FB.init({
-              appId            : '{267770262794160}',
-              xfbml            : true,
-              version          : '{18.0}'
+              appId      : '{267770262794160}',
+              cookie     : true,
+              xfbml      : true,
+              version    : '{17}'
             });
             // If you are logged in, automatically get your name and email adress, your public profile information
             FB.login(function(response) {
@@ -43,11 +37,19 @@
                     // If you are not logged in, the login dialog will open for you to login asking for permission to get your public profile and email
                     console.log('User cancelled login or did not fully authorize.'); }
             });
-        };
 
-        FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
-        });
+            (function(d, s, id){
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) {return;}
+              js = d.createElement(s); js.id = id;
+              js.src = "https://connect.facebook.net/en_US/sdk.js";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+
+            FB.getLoginStatus(function(response) {
+              statusChangeCallback(response);
+            });
+        };
 
       </script>
 
