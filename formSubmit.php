ace2a0792c8eaf06a5c86ca4b625bd5c6a29b283
Sizu24 <sizu1124@gmail.com>
2024-03-23 15:08:28 -0700

    Add html code to form success

--- formSubmit.php
+++ formSubmit.php
@@ -1,5 +1,29 @@
-<div class="form-submit">
-  <h2 class="form-submit__title">Form Submitted!</h2>
-  <p class="form-submit__body">Thank you for submitting the form. I will get back to you as soon as possible.</p>
-  <a class="form-submit__button" href="https://wealthbuildersempire.com">Back to Home</a>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Wealth Builders Empire | Form Success</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="robots" content="nofollow">
+  <meta name="description" content="Discover financial success with Wealth Builders Empire, your trusted financial advisor in Reno and Sparks Nevada. I, Matthew Smith offer expert guidance for wealth creation, investment strategies, retirement planning, and more. Let me help you build your financial empire today.">
+  <meta name="keywords" content="financial advisor, Reno financial advisors, Sparks financial advisors, wealth management, financial advisors near me, financial advisors Reno NV, investment strategies, retirement planning, financial planning, financial advisor Reno, financial advisor Sparks, financial advisor Nevada, financial advisor near me, financial advisor near Reno, financial advisor near Sparks, financial advisor near Nevada, financial advisor near me, financial advisor near me Reno, financial advisor near me Sparks, financial advisor near me Nevada, financial advisor near me, financial advisor near me Reno, financial advisor near me Sparks, financial advisor near me Nevada">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+  />
+  <link rel="stylesheet" href="./css/style.css">
+  <script src="config.js"></script>
+</head>
+  <body>
+
+    <div class="form-submit">
+      <h2 class="form-submit__title">Form Submitted!</h2>
+      <p class="form-submit__body">Thank you for submitting the form. I will get back to you as soon as possible.</p>
+      <a class="form-submit__button" href="https://wealthbuildersempire.com">Back to Home</a>
+    </div>
+
+  </body>
+</html>
