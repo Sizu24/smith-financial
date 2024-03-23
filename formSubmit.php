0555375bd90c8176d15fce9ac08e28eeee366f78
Sizu24 <sizu1124@gmail.com>
2024-03-23 15:05:45 -0700

    Add form submit styles

--- formSubmit.php
+++ formSubmit.php
@@ -1,5 +1,5 @@
 <div class="form-submit">
   <h2 class="form-submit__title">Form Submitted!</h2>
   <p class="form-submit__body">Thank you for submitting the form. I will get back to you as soon as possible.</p>
-  <a class="form-submit__button" href="index.php">Back to Home</a>
+  <a class="form-submit__button" href="https://wealthbuildersempire.com">Back to Home</a>
 </div>
