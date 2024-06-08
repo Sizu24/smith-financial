c022c6e64d68ceb8e0ec4c864699b0f135823777
Shah Chisty <sizu1124@gmail.com>
2024-06-08 13:19:27 -0700

    Add favicon

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
   <meta name="robots" content="index, follow">
   <meta name="description" content="Discover financial success with Wealth Builders Empire, your trusted financial advisor in Reno and Sparks Nevada. I, Matthew Smith offer expert guidance for wealth creation, investment strategies, retirement planning, and more. Let me help you build your financial empire today.">
   <meta name="keywords" content="financial advisor, Reno financial advisors, Sparks financial advisors, wealth management, financial advisors near me, financial advisors Reno NV, investment strategies, retirement planning, financial planning, financial advisor Reno, financial advisor Sparks, financial advisor Nevada, financial advisor near me, financial advisor near Reno, financial advisor near Sparks, financial advisor near Nevada, financial advisor near me, financial advisor near me Reno, financial advisor near me Sparks, financial advisor near me Nevada, financial advisor near me, financial advisor near me Reno, financial advisor near me Sparks, financial advisor near me Nevada">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
