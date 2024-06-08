c46cbd7f23455964b81d8e72c0a75e7cfc632dc6
Shah Chisty <sizu1124@gmail.com>
2024-06-08 12:28:14 -0700

    Add update to meta tag for crawl

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
   <title>Wealth Builders Empire</title>
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="robots" content="nofollow">
+  <meta name="robots" content="index, follow">
   <meta name="description" content="Discover financial success with Wealth Builders Empire, your trusted financial advisor in Reno and Sparks Nevada. I, Matthew Smith offer expert guidance for wealth creation, investment strategies, retirement planning, and more. Let me help you build your financial empire today.">
   <meta name="keywords" content="financial advisor, Reno financial advisors, Sparks financial advisors, wealth management, financial advisors near me, financial advisors Reno NV, investment strategies, retirement planning, financial planning, financial advisor Reno, financial advisor Sparks, financial advisor Nevada, financial advisor near me, financial advisor near Reno, financial advisor near Sparks, financial advisor near Nevada, financial advisor near me, financial advisor near me Reno, financial advisor near me Sparks, financial advisor near me Nevada, financial advisor near me, financial advisor near me Reno, financial advisor near me Sparks, financial advisor near me Nevada">
   <link rel="preconnect" href="https://fonts.googleapis.com">
